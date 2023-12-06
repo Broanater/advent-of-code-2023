da844e5eb04dea4b5745207017c92fec5788b2a0
Brody <brodyaldersley@live.com>
2023-12-06 09:34:05 -0500

    Uploading code for Day 6

---
+++ src/main/java/DaySix/Part1.java
@@ -0,0 +1,43 @@
+package DaySix;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Part1
+{
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println(Races());
+	}
+
+	private static int Races() throws IOException
+	{
+		int answer = 1;
+		List<String> input = Files.readAllLines(Paths.get("src/data/daySix/input.txt"));
+
+		String[] times = input.get(0).split("\\s+");
+		String[] distances = input.get(1).split("\\s+");
+
+		for (int counter = 0 ; counter < times.length ; counter++)
+		{
+			String timeStr = times[counter];
+			String distanceStr = distances[counter];
+			if (timeStr.contains(":")) { continue; }
+			int time = Integer.parseInt(timeStr);
+			int distance = Integer.parseInt(distanceStr);
+			int wins = 0;
+			for (int millisecs = 0 ; millisecs < time ; millisecs++)
+			{
+				int runTime = time - millisecs;
+				int travelDistance = millisecs * runTime;
+				if (travelDistance > distance) { wins++; }
+			}
+
+			answer*=wins;
+		}
+
+		return answer;
+	}
+}
---
+++ src/main/java/DaySix/Part2.java
@@ -0,0 +1,45 @@
+package DaySix;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Part2
+{
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println(Races());
+	}
+
+	private static int Races() throws IOException
+	{
+		List<String> input = Files.readAllLines(Paths.get("src/data/daySix/input.txt"));
+
+		String[] times = input.get(0).split("\\s+");
+		String[] distances = input.get(1).split("\\s+");
+
+		StringBuilder totalTimeStr = new StringBuilder();
+		StringBuilder totalDistanceStr = new StringBuilder();
+		for (int counter = 0 ; counter < times.length ; counter++)
+		{
+			String timeStr = times[counter];
+			String distanceStr = distances[counter];
+			if (timeStr.contains(":")) { continue; }
+			totalTimeStr.append(timeStr);
+			totalDistanceStr.append(distanceStr);
+		}
+
+		long totalTime = Long.parseLong(totalTimeStr.toString());
+		long totalDistance = Long.parseLong(totalDistanceStr.toString());
+		int wins = 0;
+		for (int millisecs = 0 ; millisecs < totalTime ; millisecs++)
+		{
+			long runTime = totalTime - millisecs;
+			long travelDistance = millisecs * runTime;
+			if (travelDistance >= totalDistance) { wins++; }
+		}
+
+		return wins;
+	}
+}
